9b77ce07a17484855d5e02a0f38847c56205fd6d
Linus <liinus.wiberg@gmail.com>
2025-02-25 17:10:33 +0200

    Fix: jTrigger not working correctly due to not making the toggled element run the "mousedown" event.

--- src/toggle.ts
+++ src/toggle.ts
@@ -93,6 +93,7 @@ function getTrigger() {
     const targets = getHTMLTargets(trigger, "jtrigger");
     trigger.addEventListener("click", () => {
       targets.forEach((target) => {
+        target.dispatchEvent(new MouseEvent("mousedown", { bubbles: true, cancelable: true }));
         target.click();
       });
     });
